a94e9573cad01cc393020bb6afdbfb611ee2e48d
SaeedKarimian860 <SGK12190@gmail.com>
2025-02-05 23:36:59 -0500

    JavaScript Review

--- script.js
+++ script.js
@@ -175,8 +175,10 @@ const updatedBook = {
 };
 updatedBook;
 
-const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${publicationDate.split("-")[0]}`;
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${publicationDate.split("-")[0]
+}, the book has ${hasMovieAdaptation ? '' : 'not'} been adapted as a movie`;
 summary;
 
 const pagesRange = pages > 1000 ? 'over a thousand' : 'less than 1000';
 pagesRange;
+console.log(`the book has ${pagesRange} pages`);
